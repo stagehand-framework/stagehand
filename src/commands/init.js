12c23a16c365a7fdaaffc46bcd802235df71a8ec
Emil Reji <emil315.reji@gmail.com>
2020-11-13 11:39:25 -0500

    Changed args to hash Co-authored-by: Cody Stair <ccstair27@gmail.com> Co-authored-by: Daniel Lew <danielshlomolew@gmail.com>

--- src/commands/init.js
+++ src/commands/init.js
@@ -1,62 +1,67 @@
-const { exec } = require('child_process');
-const { stagehandErr, stagehandLog } = require('../util/logger');
-const parseStackOutputs = require('../util/parseStackOutputs');
+const { exec } = require("child_process");
+const { stagehandErr, stagehandLog } = require("../util/logger");
+const parseStackOutputs = require("../util/parseStackOutputs");
 
 // const stackName = 'stagehand-init-stack2'; This will be gotten from args[1]
 
-const getTemplatePath = (ssg) => `./templates/${ssg}/cloudformation_template.yml`;
+const getTemplatePath = (ssg) =>
+  `./templates/${ssg}/cloudformation_template.yml`;
 
 const createStackCmd = (templatePath, stackName) => {
   return `aws cloudformation deploy --template-file ${templatePath} --stack-name ${stackName} --capabilities CAPABILITY_IAM`;
-}
-const getStackOutputs = (stackName) => `aws cloudformation describe-stacks --stack-name ${stackName}`;
+};
+const getStackOutputs = (stackName) =>
+  `aws cloudformation describe-stacks --stack-name ${stackName}`;
 
 // const logCmd = (error, stdout, stderr) => {
 //   if (error) {
 //     stagehandErr(`error: ${error.message}`);
 //     return;
 //   }
-  
+
 //   if (stderr) {
 //     stagehandErr(`stderr: ${stderr}`);
 //     return;
 //   }
-  
+
 //   stagehandLog(`stdout: ${stdout}`);
 // }
 
 const createStagehand = (ssg, stackName) => {
   return new Promise((resolve, reject) => {
-    exec(createStackCmd(getTemplatePath(ssg), stackName), (error, stdout, stderr) => {
-      if (error) {
-        stagehandErr(`error: ${error.message}`);
-        return;
-      }
-      
-      if (stderr) {
-        resolve(stagehandErr(`stderr: ${stderr}`));
-        return;
-      }
-      
-      resolve(stagehandLog(`stdout: ${stdout}`));
-    });
-  })
-}
-
-const init = async (args) => {
-  try {
-    createStagehand(args[0], args[1]).then(resolve => {
-      exec(getStackOutputs(args[1]), (error, stdout, stderr) => {
+    exec(
+      createStackCmd(getTemplatePath(ssg), stackName),
+      (error, stdout, stderr) => {
         if (error) {
           stagehandErr(`error: ${error.message}`);
           return;
         }
-        
+
+        if (stderr) {
+          resolve(stagehandErr(`stderr: ${stderr}`));
+          return;
+        }
+
+        resolve(stagehandLog(`stdout: ${stdout}`));
+      }
+    );
+  });
+};
+
+const init = async (args) => {
+  try {
+    createStagehand(args["ssg"], args["stackName"]).then((resolve) => {
+      exec(getStackOutputs(args["stackName"]), (error, stdout, stderr) => {
+        if (error) {
+          stagehandErr(`error: ${error.message}`);
+          return;
+        }
+
         if (stderr) {
           stagehandErr(`stderr: ${stderr}`);
           return;
         }
-        
+
         const outputs = parseStackOutputs(stdout);
         stagehandLog(outputs);
       });
--- src/util/handleArgs.js
+++ src/util/handleArgs.js
@@ -1,23 +1,53 @@
 const { stagehandErr, stagehandLog } = require("../util/logger");
 const ssgs = ["gatsby", "next", "hugo"];
+const keys = ["ssg", "stackName"];
+
+const getRandomIntInclusive = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive
+};
+
+const generateRandomStackName = (len) => {
+  let i = 0;
+  result = "";
+
+  while (i < len) {
+    result += String.fromCharCode(getRandomIntInclusive(97, 122));
+    i += 1;
+  }
+
+  return result;
+};
 
 module.exports = function handleArgs(args) {
-  const filteredArgs = args.map((arg) => {
-    return arg.replace(/-/g, "");
+  const filteredArgs = {};
+
+  args.map((arg, idx) => {
+    const key = keys[idx];
+    const val = arg.replace(/-/g, "");
+    filteredArgs[key] = val;
   });
 
   if (filteredArgs.length === 0) {
     throw new Error("You have failed to provide any arguments!");
   }
 
-  if (!ssgs.includes(filteredArgs[0])) {
+  if (!ssgs.includes(filteredArgs["ssg"])) {
     throw new Error(
       "You have failed to provide a valid static site generator!"
     );
   }
 
+  if (!filteredArgs["stackName"]) {
+    filteredArgs["stackName"] = generateRandomStackName(10);
+    stagehandLog(
+      `You have failed to provide a stack name. Creating a random one: ${filteredArgs["stackName"]}`
+    );
+  }
+
   stagehandLog(
-    `Building stagehand for this repo which uses the ${filteredArgs[0]} SSG`
+    `Running stagehand for this repo which uses the ${filteredArgs["ssg"]} SSG. The stack name will be ${filteredArgs["stackName"]}.`
   );
 
   return filteredArgs;
