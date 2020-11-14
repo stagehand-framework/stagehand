156d60bf7415ffe09ec5390ef54d2171335bf3df
Daniel Lew <danielshlomolew@gmail.com>
2020-11-14 16:26:14 -0500

    added list command and added a couple todos
    Co-authored-by: Cody Stair <ccstair27@gmail.com>
    Co-authored-by: Emil Reji <emil315.reji@gmail.com>

---
+++ src/commands/list.js
@@ -0,0 +1,26 @@
+const { stagehandErr, stagehandLog } = require('../util/logger');
+const { dataPath } = require('../util/path');
+
+module.exports = async function list(args) {
+  const rawUserAppsData = fs.readFileSync(dataPath);
+  const userApps = JSON.parse(rawUserAppsData);
+  const appName = args; // TODO: fix handleArgs so we can grab appName from it
+
+  if (args.length === 0) {
+    const stackNames = Object.keys(userApps);
+    const stackNamesStr = stackNames.map((stackName) => {
+      return `--- Name: ${stackName}`;
+    }).join('\n');
+
+    stagehandLog(`Current Active Stagehand Apps
+      ---------------------------
+      ${stackNamesStr}
+      ---------------------------
+    `);
+
+  } else if (userApps[appName]) {
+    // TODO: Grab info about stack
+  } else {
+    stagehandLog(`No stagehand with name ${appName} found`)
+  }
+};
