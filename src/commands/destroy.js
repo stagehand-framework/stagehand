82214eb41a990662f90bf11da2517e451c289d3d
Cody Stair <ccstair27@gmail.com>
2020-11-17 12:27:18 -0500

    destroy deletes stack from cloud formation
    Co-authored-by: Daniel Lew <danielshlomolew@gmail.com>
    Co-authored-by: Emil Reji <emil315.reji@gmail.com>

--- src/commands/destroy.js
+++ src/commands/destroy.js
@@ -2,36 +2,34 @@ const { stagehandErr, stagehandLog } = require("../util/logger");
 const readlineSync = require("readline-sync");
 const { readDataFile, writeToDataFile } = require("../util/fs");
 const userApps = readDataFile();
-
-userApps.stack2 = {
-  s3: "mystack-s3bucket-7koqeoo104jn",
-  domain: "d135hwjxj512vl.cloudfront.net",
-  region: "us-east-1",
-  id: "E1R75VSQK790XS",
-};
+const { deleteStack } = require("../aws/deleteStack");
+const { wrapExecCmd } = require("../util/wrapExecCmd");
 
 const validateDestroy = (args) => {
-  stagehandLog(args);
   stagehandLog("Are you sure you want to destroy ths app?");
-
   const answer = readlineSync.question(
     "Type the stack name if you are sure:  "
   );
 
-  if (Object.keys(userApps).includes(answer)) {
-    deleteAppFromDataFile(answer);
-  }
-
-  stagehandLog(userApps);
+  return Object.keys(userApps).includes(answer);
 };
 
 const deleteAppFromDataFile = (stackName) => {
   delete userApps[stackName];
 };
 
+const deleteStackResources = (stackName) => {
+  const cmd = deleteStack(stackName);
+
+  wrapExecCmd(cmd).then((_) => {
+    deleteAppFromDataFile(stackName);
+  });
+};
+
 const destroy = async (args) => {
-  stagehandLog(userApps);
-  validateDestroy(args);
+  if (validateDestroy(args.stackName)) {
+    deleteStackResources(args.stackName);
+  }
 };
 
 module.exports = { destroy };
