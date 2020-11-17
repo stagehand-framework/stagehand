ad39896a9df0c6ac2963998ab1fe0553d579029f
Daniel Lew <danielshlomolew@gmail.com>
2020-11-17 13:40:38 -0500

    Added help command
    Co-authored-by: Cody Stair <ccstair27@gmail.com>
    Co-authored-by: Emil Reji <emil315.reji@gmail.com>

--- src/util/logger.js
+++ src/util/logger.js
@@ -2,6 +2,8 @@ const escape = "\x1b";
 const reset = "\x1b[0m";
 const red = "[31m";
 const yellow = "[33m";
+const green = "[32m";
+const help = "[1;36m";
 
 const stagehandErr = (text) => {
   console.log(`Error ${escape}${red}`, text, reset);
@@ -15,8 +17,18 @@ const stagehandLog = (text) => {
   console.log(text);
 };
 
+const stagehandHelp = (text) => {
+  console.log(`${escape}${help}`, text, reset);
+};
+
+const stagehandSuccess = (text) => {
+  console.log(`${escape}${green}`, text, reset);
+};
+
 module.exports = {
   stagehandErr,
   stagehandWarn,
   stagehandLog,
+  stagehandHelp,
+  stagehandSuccess,
 };
