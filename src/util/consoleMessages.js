b79f0f8799802445080dc3a260f7aa5575c0cf9d
Daniel Lew <danielshlomolew@gmail.com>
2020-11-17 15:52:30 -0500

    Fix help msg formatting
    Co-authored-by: Cody Stair <ccstair27@gmail.com>
    Co-authored-by: Emil Reji <emil315.reji@gmail.com>

--- src/util/consoleMessages.js
+++ src/util/consoleMessages.js
@@ -4,36 +4,45 @@ const commands = ["help", "init", "list", "destroy"];
 
 const initHelp = `
 usage: stagehand init --build buildOption [--stackName name]
+
 Make sure you have configured the AWS CLI first.
 Must be called from the root of your git repository.
 If no name is provided a random one will be generated.
+
 Available build options: ${builds.join(", ")}
 Check here for config details for builds: <TODO: insert README website>
+
 Examples: stagehand init --build gatsby --stackName myName
           stagehand init --build react
 `;
 
 const listHelp = `
 usage: stagehand list [--stackName myName]
+
 List with no name will return a list of all your stacks.
 List with a name will return all available review apps for specific stack.
+
 Examples: stagehand list
           stagehand list --stackName myName 
 `;
 
 const destroyHelp = `
 usage: stagehand destroy --stackName myName
+
 This will tear down the AWS infrastructure associated with the app.
 It will also remove associated github action files in your repo.
 Make sure to push changes to your repo so the actions won't be triggered on GitHub.
+
 Example: stagehand destroy --stackName myName 
 `;
 
 const noCommandHelp = `
 usage: stagehand <command> [parameters, ...]
+
 To see help text, you can run:
   stagehand help --<command>
   available commands: ${commands.join(", ")}
+
 Example: stagehand help --init
 `;
 
