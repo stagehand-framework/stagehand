9a7c7ebc0f35231360c62520246baa54248f049b
Daniel Lew <danielshlomolew@gmail.com>
2020-11-23 15:01:25 -0500

    Refactor service worker and fix navigation issues
    Co-authored-by: Cody Stair <ccstair27@gmail.com>
    Co-authored-by: Emil Reji <emil315.reji@gmail.com>

--- templates/stagehand/stagehand_js.js
+++ templates/stagehand/stagehand_js.js
@@ -2,26 +2,53 @@ window.addEventListener("DOMContentLoaded", function (e) {
   console.log(e);
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker
-      .register("https://STAGEHAND_PLACEHOLDER/stagehand_sw.js")
+      .register("https://d3t33jqvgjin33.cloudfront.net/stagehand_sw.js")
       .catch(err => console.error("Service worker registration failed", err));
 
     const iframe = document.querySelector("iframe");
-
-    const path = window.location.href.split("#")[1];
     const basepath = window.location.pathname;
-
-    iframe.src = basepath + (path || "index") + ".html";
-
+    const path = window.location.href.split("#")[1];
+    
+    let iframePolling;
     let iframePath;
 
-    setInterval(() => {
-      iframePath = (
-        iframe.contentWindow || iframe.contentDocument
-      ).location.pathname.slice(1);
-      if (window.location.hash.slice(1) !== iframePath) {
-        window.history.pushState({}, "iframe", `#${iframePath}`);
-      }
-    }, 500);
+    const polliFrame = () => {
+      return setInterval(() => {
+        iframePath = (
+          iframe.contentWindow || iframe.contentDocument
+        ).location.pathname.slice(1) || 'index';
+        
+        const tempiFramePath = '/' + iframePath;
+
+        if ((tempiFramePath).match(basepath)) {
+          iframePath = tempiFramePath.split(basepath)[1];
+        }
+
+        if (iframePath.endsWith('.html')) iframePath = iframePath.slice(0, -5);
+
+        if (window.location.hash.slice(1) !== iframePath) {
+          console.log('new poll:', iframePath)
+          const iframeTitle = (iframe.contentWindow || iframe.contentDocument).document.title;
+          window.history.pushState({}, iframeTitle, `#${iframePath || 'index'}`);
+        }
+      }, 500);
+    }
+
+    console.log('change location: ', basepath + (path || 'index') + '.html')
+    if (path && path[0] === '/') path = path.slice(1);
+
+    iframe.src = basepath + (path || "index") + ".html";
+    iframePolling = polliFrame();
+
+    window.addEventListener('popstate', function(e) {
+      clearInterval(iframePolling);
+      const newPath = window.location.hash.slice(1) || 'index';
+
+      console.log(basepath + newPath + '.html')
+      iframe.src = basepath + newPath + '.html';
+
+      iframePolling = polliFrame();
+    });
 
     console.log(navigator.serviceWorker);
   } else {
--- templates/stagehand/stagehand_sw.js
+++ templates/stagehand/stagehand_sw.js
@@ -10,147 +10,70 @@ caches.keys().then(function (names) {
   for (let name of names) caches.delete(name);
 });
 
-// const channel = new BroadcastChannel("stagehand");
 let basePath;
-
-// let client;
 
 const handleRequest = async event => {
   console.log(event);
-  // Send a message to the client
 
   let requestedUrl;
-  // if referrer matches the url and has a mode navigate
-  // intercept event and send the url over postMessage
+
   const url = event.request.url;
   const referrer = event.request.referrer;
-  // client = client || event.clientId;
 
-  // Get domain of current url and requested url
+  // Get CF domain of current url and requested url
+  // => "https://124125.cloudfront.net"
   const urlDomain = url.match(/[^\/]+\/\/[^\.]+\.cloudfront\.net/);
   const referrerDomain = referrer.match(/[^\/]+\/\/[^\.]+\.cloudfront\.net/);
 
   // Get basepath of referrer
+  // => ["125125.cloudfront.net", "branch/commit"]
   const referrerBasePathMatch = referrer.match(
     /[^\/]+\/\/[^\.]+\.cloudfront.net\/([^\/]+\/[^\/]+)/
   );
 
   // Get basepath and remaining path of requested url
-  const urlPathMatch = url.match(
-    /[^\/]+\/\/[^\.]+\.cloudfront.net\/([^\/]+\/[^\/]+)\/(.*)/
-  );
+  // => "about/index"
+  const urlPathMatch = url.match(/[^\/]+\/\/[^\.]+\.cloudfront.net\/(.*)/);
 
-  // Base paths
-  const referrerBasePath =
-    (referrerBasePathMatch && referrerBasePathMatch[1]) || "";
-  const urlBasePath = (urlPathMatch && urlPathMatch[1]) || "";
+  // => "branch/commit"
+  basePath = basePath || (referrerBasePathMatch && referrerBasePathMatch[1]) || '';
 
-  basePath = basePath || referrerBasePath;
-
-  console.log(referrerBasePath, urlBasePath);
-  // check if the domains are both for cloudfront and the request is for an html doc
-  if (
-    (referrerDomain && referrerDomain[0]) === (urlDomain && urlDomain[0]) &&
-    event.request.mode === "navigate"
-  ) {
-    // Build up domain and base path
-    requestedUrl = referrerDomain[0] + "/" + referrerBasePath;
-    console.log(requestedUrl);
+  console.log('basePath:', basePath);
+  // check if the domains are both for same cloudfront
+  // If they are we need to make sure it has basePath prepended
+  if ((referrerDomain && referrerDomain[0]) === (urlDomain && urlDomain[0])) {
+    let urlPath = urlPathMatch[1] || '';
     let req;
-    // If base paths are different that means the url has no base path
-    // Add on everything after the cloudfront domain to requestedUrl
-    if (referrerBasePath !== urlBasePath) {
-      const path = url.split("cloudfront.net");
-      requestedUrl += "/" + (path && path[1]);
 
-      // // respond with nothing to iframe
-      const controller = new AbortController();
-      const signal = controller.signal;
+    if (urlPath.startsWith('/')) urlPath = urlPath.slice(1);
+    if (urlPath && !urlPath.includes('.')) urlPath += '.html';
 
-      req = new Request(requestedUrl, {
-        mode: "cors",
-        signal,
-      });
+    // if it already has basepath send req as is
+    if (urlPath.match(basePath)) {      
+      urlPath = urlDomain + '/' + urlPath
 
-      // if the base paths are the same that means that the iframe was updated by the JS script
-      // Make a new request using the proper URL
+    // prepend basepath
     } else {
-      if (!referrerBasePath) console.log(requestedUrl, urlPathMatch);
-      requestedUrl += "/" + urlPathMatch[2] || "";
-      req = new Request(requestedUrl, {
-        mode: "cors",
-        cache: event.request.cache,
-        headers: event.request.headers,
-        referrer: event.request.referrer,
-      });
+      urlPath = urlDomain + '/' + basePath + '/' + urlPath;
     }
+
+    console.log('urlPath:', urlPath);
+
+    req = new Request(urlPath, {
+      ...event.request,
+    });
+
+    console.log("request:", req);
 
     return fetch(req)
       .then(res => res)
       .catch(err => console.log("err:", err));
-
-    // to handle everything that isn't HTML (navigation)
-  } else if (
-    (referrerDomain && referrerDomain[0]) === (urlDomain && urlDomain[0])
-  ) {
-    // if basepath is there, do nothing, if basepath isn't there, add it
-    if (referrerBasePath !== urlBasePath) {
-      // Inject basepath to url
-
-      let newBasePath;
-      if (referrerBasePath) newBasePath = referrerBasePath + "/";
-      const newUrl = url.replace(
-        "cloudfront.net/",
-        `cloudfront.net/${basePath + "/" || ""}`
-      );
-
-      console.log(newUrl);
-      const req = new Request(newUrl, {
-        mode: "cors",
-        cache: event.request.cache,
-        headers: event.request.headers,
-        referrer: event.request.referrer,
-      });
-
-      console.log("request:", req);
-
-      return fetch(req)
-        .then(res => res)
-        .catch(err => console.log("err:", err));
-
-    } else if (!referrerBasePath) {
-      const newUrl = url.replace(
-        "cloudfront.net/",
-        `cloudfront.net/${basePath + "/" || ""}`
-      );
-
-      const req = new Request(newUrl, {
-        mode: "cors",
-        cache: event.request.cache,
-        headers: event.request.headers,
-        referrer: event.request.referrer,
-      });
-
-      console.log("request:", req);
-
-      return fetch(req)
-        .then(res => res)
-        .catch(err => console.log("err:", err));
-    }
   }
 
-  const req = new Request(event.request.url, {
-    ...event.request,
-    mode: "cors",
-    cache: event.request.cache,
-    headers: event.request.headers,
-    referrer: event.request.referrer,
-  });
-
-  return fetch(req)
+  // if not origin domain then just forward the initial request
+  return fetch(event.request)
     .then(res => res)
     .catch(err => console.log("err:", err));
-  console.log("in the postMessage");
 };
 
 self.addEventListener("fetch", event => {
