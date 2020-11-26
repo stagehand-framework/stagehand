1876485255da57321dd06905b8d96da6fa64ad77
Daniel Lew <danielshlomolew@gmail.com>
2020-11-26 16:06:53 -0500

    Clean up access command and add functionality to go back in choices
    Co-authored-by: Cody Stair <ccstair27@gmail.com>
    Co-authored-by: Emi Reji <emil315.reji@gmail.com>

--- src/aws/putBucketAcl.js
+++ src/aws/putBucketAcl.js
@@ -1,7 +1,10 @@
-const putBucketAcl = (bucket, ids) => {
+const putBucketAcl = (bucket, ids, email) => {
   const idsStr = ids.map(id => `id=${id}`).join(',');
-  
-  return `aws s3api put-bucket-acl --bucket ${bucket} --grant-full-control ${idsStr}`;
+  let newAccessStr;
+
+  if (email) newAccessStr = idsStr.concat(`,emailaddress=${email}`);
+
+  return `aws s3api put-bucket-acl --bucket ${bucket} --grant-full-control ${newAccessStr || idsStr}`;
 }
 
 module.exports = {
--- src/commands/access.js
+++ src/commands/access.js
@@ -14,115 +14,108 @@ const {
   displayListMessage,
   noAppFoundMessage,
 } = require("../util/consoleMessages");
+const userApps = readDataFile();
+const commands = ["ADD", "REMOVE", "VIEW"];
 
-const invalidIdMessage = `Please provide a valid id:
-          Get stagehand id of user you wish to give access to: "stagehand get-id"
-          Use their id in command: "stagehand access --add --stackName <name> --id <id>"`;
+const chooseAccessCommands = async (stackNames) => {
+  const questions = [
+    {
+      type: "select",
+      name: "stackName",
+      message: `Select a stagehand app to apply access controls`,
+      choices: stackNames,
+    },
+    {
+      type: "select",
+      name: "access",
+      message: `Select access control`,
+      choices: commands.concat('GO BACK TO APP LIST'),
+    },
+  ];
+
+  const result = await prompts(questions);
+  const stackName = stackNames[result["stackName"]];
+  const command = commands[result["access"]];
+
+  console.log(command);
+  if (Object.keys(result).length < 2) return;
+  
+  return (!command)
+    ? await chooseAccessCommands(stackNames) 
+    : { stackName, command };
+};
 
 const access = async () => {
-  const userApps = readDataFile();
-  const stackNames = Object.keys(userApps);
-  stackNames.splice(stackNames.indexOf("to_delete"), 1);
-  const commands = ["add", "remove", "view"];
-  if (stackNames.length === 0) {
-    stagehandWarn(
-      `No stagehand apps have been created or added\n Start with "stagehand help --init"`
-    );
-  } else {
-    const questions = [
-      {
-        type: "select",
-        name: "stackName",
-        message: `Please enter the stack name for which access is being applied:`,
-        choices: stackNames,
-      },
-      {
-        type: "select",
-        name: "access",
-        message: `What do you want to do with the stagehand access:`,
-        choices: commands,
-      },
-      {
-        type: (command_idx) =>
-          commands[command_idx] === "view" ? null : "text",
-        name: "person",
-        message: "Please enter the AWS canonical id or an AWS username:",
-      },
-    ];
+  try {
+    const stackNames = Object.keys(userApps);
 
-    const result = await prompts(questions);
-    console.log(result);
-    const stackName = stackNames[result["stackName"]];
-    const command = commands[result["access"]];
-
-    const bucket = userApps[stackName].s3;
-    const currUserAccess = await wrapExecCmd(getBucketAcl(bucket));
-    const bucketPolicy = JSON.parse(currUserAccess);
-    const ids = bucketPolicy.Grants.map((grant) => grant.Grantee.ID);
-
-    if (command === "view") {
-      const users = bucketPolicy.Grants.map(
-        (grant) => grant.Grantee.DisplayName
+    if (stackNames.length === 0) {
+      stagehandWarn(
+        `No stagehand apps have been created or added\n Start with "stagehand help --init"`
       );
+    } else {
+      const { stackName, command } = await chooseAccessCommands(stackNames);
 
-      const filteredUsers = users.filter((user, idx) => {
-        return users.indexOf(user, idx + 1) === -1;
-      });
+      const bucket = userApps[stackName].s3;
+      const currUserAccess = await wrapExecCmd(getBucketAcl(bucket));
+      const bucketPolicy = JSON.parse(currUserAccess);
+      
+      const users = bucketPolicy.Grants.map((grant) => grant.Grantee.DisplayName);
+      const ids = bucketPolicy.Grants.map((grant) => grant.Grantee.ID);
+      const owner = bucketPolicy.Owner.DisplayName;
+      const usersWithOwner = users.map(user => user === owner ? `${user} [OWNER]` : user);
 
-      displayListMessage(`Users with access to ${stackName}`, filteredUsers);
-    } else if (command === "add") {
-      if (!id) return stagehandErr(invalidIdMessage);
+      if (command === "VIEW") {
+        displayListMessage(`Users with access to ${stackName}`, usersWithOwner);
+      
+      } else if (command === "ADD") {
+        const question = {
+          type: 'text',
+          name: "email",
+          message: "Enter the email associated to the users AWS account:",
+          validate: email => !email.match(/[^@]+@[^\.]+\.[^@\.]+/) ? 'Please provide a valid email address' : true,
+        }
 
-      if (ids.includes(id)) {
-        user = bucketPolicy.Grants.find((grant) => grant.Grantee.ID === id)
-          .Grantee.DisplayName;
-        return stagehandWarn(`${user} already has access to ${stackName}`);
+        const result = await prompts(question);
+        const email = result["email"];
+        const user = email.split('@')[0];
+
+        if (users.includes(user)) {
+          return stagehandWarn(`${user} already has access to ${stackName}`);
+        }
+
+        stagehandWarn(`Adding access for ${user} for ${stackName}`);
+
+        await wrapExecCmd(putBucketAcl(bucket, ids, email));
+        
+        stagehandSuccess("added", `Access for ${user} for ${stackName}:`);
+        stagehandSuccess(bucket, `Share this bucket identifier with ${user} to pass to "stagehand add":`);
+      
+      } else if (command === "REMOVE") {
+        const selectList = {
+          type: 'select',
+          name: 'user',
+          message: 'Select the user to remove access:',
+          choices: usersWithOwner,
+        };
+
+        const result = await prompts(selectList);
+        const user = users[result.user].split(' ')[0];
+        if (user === owner) return stagehandWarn('Cannot remove access from owner');
+        
+        const id = bucketPolicy.Grants.find(
+            (grant) => grant.Grantee.DisplayName === user
+          ).Grantee.ID;
+
+        const newIds = ids.filter((userId) => userId !== id);
+
+        await wrapExecCmd(putBucketAcl(bucket, newIds))
+        
+        stagehandSuccess("removed", `Access for user ${user} for ${stackName}:`);
       }
-
-      stagehandWarn(`Adding access for user ${id} for ${stackName}`);
-
-      wrapExecCmd(putBucketAcl(bucket, [...ids, id]))
-        .then((output) => {
-          stagehandSuccess("added", `Access for user ${id} for ${stackName}:`);
-          const addCmd = `stagehand add --bucket ${bucket}`;
-          stagehandSuccess(
-            addCmd,
-            "Copy this command and pass to user to add stagehand stack locally:"
-          );
-        })
-        .catch((err) => stagehandErr(`Invalid id provided`));
-    } else if (command === "remove") {
-      if (!id && !user)
-        return stagehandErr(`Please provide a valid --id or --userName`);
-
-      const userNames = bucketPolicy.Grants.map(
-        (grant) => grant.Grantee.DisplayName
-      );
-
-      if ((id && !ids.includes(id)) || !userNames.includes(user)) {
-        return stagehandWarn(
-          `${id || user} doesn't have access to ${stackName}`
-        );
-      }
-
-      if (!id)
-        id = bucketPolicy.Grants.find(
-          (grant) => grant.Grantee.DisplayName === user
-        ).Grantee.ID;
-      if (!user)
-        user = bucketPolicy.Grants.find((grant) => grant.Grantee.ID === id)
-          .Grantee.DisplayName;
-      const newIds = ids.filter((userId) => userId !== id);
-
-      wrapExecCmd(putBucketAcl(bucket, newIds))
-        .then((output) =>
-          stagehandSuccess(
-            "removed",
-            `Access for user ${user} for ${stackName}:`
-          )
-        )
-        .catch((err) => stagehandErr(`Invalid id provided`));
     }
+  } catch {
+    stagehandErr('Could not apply access controls');
   }
 };
 
