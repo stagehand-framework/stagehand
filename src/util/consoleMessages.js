73c9613397fbbb28d6c7ab2ebdf21629c3398100
Emil Reji <emil315.reji@gmail.com>
2020-11-30 14:38:07 -0500

    Updating help output
    Co-authored-by: Cody Stair <ccstair27@gmail.com>
    Co-authored-by: Daniel Lew <danielshlomolew@gmail.com>

--- src/util/consoleMessages.js
+++ src/util/consoleMessages.js
@@ -2,106 +2,61 @@ const { stagehandLog, stagehandSuccess } = require("./logger");
 
 // ******** help Command ********
 const builds = ["next", "hugo", "gatsby", "react"];
-const commands = ["help", "init", "list", "destroy", "access", "add", "getId"];
+const commands = ["help", "init", "list", "destroy", "access", "add"];
 
 const initHelp = `
-usage: stagehand init --build buildOption [--stackName name]
-
-Make sure you have configured the AWS CLI first.
-Must be called from the root of your git repository.
-If no name is provided a random one will be generated.
-
-Available build options: ${builds.join(", ")}
-Check here for config details for builds: <TODO: insert README website>
-
-Examples: stagehand init --build gatsby --stackName myName
-          stagehand init --build react
+Run this command from the root of your repostory that is linked to a GitHub repository.
+Follow the prompts to create a new Stagehand application.
+Provide your GitHub Token (this token only needs to be provided the very first use).
+A folder in your home directory /.stagehand will be created to house your configuration and your application data for Stagehand.
+Then you will be prompted to provide:
+- The name you wish to use.
+- The setup command your app uses (ie npm install, brew install hugo).
+- The build command your app uses (ie npm run-script build, hugo).
+- The directory that your app builds to (ie public, out, build).
+This will create a .github/workflow directory in your repo for GitHub Actions.
+If you need to alter your GitHub build process look for create_review_app.yml and remove_review_app.yml.
+The last step is pushing your ./github folder to your repository.
+You are now ready to start using Stagehand.
 `;
 
 const listHelp = `
-usage: stagehand list [--stackName myName]
-
-List with no name will return a list of all your stacks.
-List with a name will return all available review apps for specific stack.
-
-Examples: stagehand list
-          stagehand list --stackName myName 
+This will show all the Stagehand applications that you currently are involved with.
+Once you select a Stagehand application you will be shown a list of current staging environments that exist.
+Open them up by selecting and pressing enter.
 `;
 
 const destroyHelp = `
-usage: stagehand destroy --stackName myName
-
-This will tear down the AWS infrastructure associated with the app.
-It will also remove associated github action files in your repo.
-Make sure to push changes to your repo so the actions won't be triggered on GitHub.
-
-Example: stagehand destroy --stackName myName 
+First you select which Stagehand application you wish to remove
+If you are the owner of the Stagehand application:
+This command will remove:
+- Stagehand related files and folders from your repository
+- AWS infrastructure
+- Application data from the local datastore
+- AWS Secrets from your GitHub repository
+If you are not the owner of the application (it was added using stagehand add):
+- This command will just remove application data from the local datastore
 `;
 
 const accessHelp = `
-usage: stagehand access --[add|view|remove|] --stackName <name> --id <id>
-
-This will add to, remove from, or view the access control of a stagehand app.
-
-For adding:
-- First the user you want to add has to provide their AWS canonical id (see stagehand help --getId).
-- Then when you run the following command with their id, they will have access to the app's S3 bucket.
-- This will allow the user to add this app to their own set of apps (see stagehand help --add).
-For removing:
-- The same rules apply as adding. You can use the canonical id to remove a user.
-- However you can also remove based on a username (usually up to the @ in the user's email).
-For viewing:
-- This will output all users who have access to a specified stagehand app.
-- Note the usernames are provided (hence making it easier to determine who to remove).
-
-Example: stagehand access --add --stackName stackName --id 1234567890
-Example: stagehand access --remove --stackName stackName --id 1234567890
-Example: stagehand access --remove --stackName stackName --userName john.smith
-Example: stagehand access --view --stackName stackName
+Use this command to VIEW, ADD, or REMOVE access to one of your applications
+ADD access lets other developers working on the same repository have access to the active staging environments
+To ADD access you must have the user's AWS Account Email
+The ADD command will return the name of the storage location of the staging environments
 `;
 
 const addHelp = `
-usage: stagehand add --bucket <bucket>
-
-This will add an existing stagehand app to your data file.
-A S3 bucket name will need to be provided by the owner of the stagehand app.
-Note that the owner will need to be give you permission to access this app (see stagehand help --access).
-For them to do this, you should provide them your AWS canonical id (see stagehand help --getId).
-Once this is done, you should be able to run commands with that stagehand app. 
-
-Example: stagehand add --bucket bucketName
-`;
-
-const getIdHelp = `
-usage: stagehand getId
-
-This will return your AWS canonical id which serves as your unique identifier.
-A valid AWS account will auto create this.
-This id can be provided to another user of stagehand so they can add you to their application (see stagehand help --access).
-
-Example: stagehand getId
-`;
-
-const cleanupHelp = `
-usage: stagehand cleanup
-
-This will remove all persistent lambdas from previously destroyed stagehand apps.
-Lambdas cannot be deleted until all instances of the lambda across cloudfront have been deleted.
-Due to the long wait time required for this, the actual destroy command will not remove the lambda.
-Instead, destroy will make the lambda ready for cleanup by this command.
-Once you have waited at least 30 minutes post destruction, this cleanup command can be run.
-
-Example: stagehand cleanup
+First you provide a user access to your Stagehand application using stagehand access => ADD
+The user you provided access to must input the name of the storage location provided from the previous command
+This will give you access to the application when you run stagehand list
 `;
 
 const noCommandHelp = `
-usage: stagehand <command> [parameters, ...]
-
-To see help text, you can run:
-  stagehand help --<command>
-  available commands: ${commands.join(", ")}
-
-Example: stagehand help --init
+To see general help, you can run:
+  stagehand -h
+  stagehand --help
+To see help for an individual command, you can run:
+  stagehand help
 `;
 
 const helpLogs = {
@@ -111,8 +66,6 @@ const helpLogs = {
   help: noCommandHelp,
   access: accessHelp,
   add: addHelp,
-  getId: getIdHelp,
-  cleanup: cleanupHelp,
 };
 
 const lambdaDeleteErrorMessage = (lambda) => {
@@ -155,8 +108,8 @@ To build out a custom dashboard check out the "/.github/stagehand.html" file
 Your stagehand environment build process is located at "/.github/workflows/create_review_app".
 This file will get triggered on a Pull Request, and on any additional commits to the branch.
 
-`
-}
+`;
+};
 
 // ******** list Command ********
 const displayListMessage = (title, list) => {
